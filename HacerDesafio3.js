5fd0a72795a30a667ddc21b80d21763accc1adb1
CristianGodoy81 <cristiangodoy81@gmail.com>
2023-09-30 23:26:13 -0300

    Ingreso los desafios

---
+++ HacerDesafio3.js
@@ -0,0 +1,28 @@
+/*
+Desafío extra (opcional):
+
+Instrucciones
+Si llegaste hasta acá, vienes trabajando muy bien poniendo en práctica los diferentes
+contenidos vistos hasta el momento. ¡Felicitaciones!
+
+Para que no te quedes con las ganas y puedas seguir practicando, te proponemos que
+desarrolles las siguientes funciones. No olvides verificar los valores resultantes utilizando la
+instrucción console.log().
+
+1. En la carpeta raíz del proyecto, crear un nuevo archivo (extra.js).
+2. Crear un array de números y asignarle un total de diez (10) valores numéricos.
+3. Crear variables que contengan los valores numéricos dispuestos en el array en la
+posición (0, 2 y 4) y con el resto de los valores numéricos crear un nuevo array. Utiliza
+los recursos aprendidos en esta clase: Destructuring y Spread operator.
+4. Crear un objeto literal para almacenar los datos de nuestra mascota, considerando
+los atributos:
+a. nombre
+b. tipo de mascota (Perro - Gato)
+c. color
+d. raza
+Haciendo uso de la desestructuración, crea una variable por cada una de las
+claves del objeto literal y al final mostrar al usuario:
+
+Hola les presento a mi mascota su nombre es: Bony, es un hermoso Perro, de
+color: Dorado y su raza es: Golden retriever.
+*/
